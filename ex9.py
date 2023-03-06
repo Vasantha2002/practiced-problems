c0bd0ba97f45f28a17378328269b2df4aaaf942b
Vasantha2002 <vasanthamedisetti0806@gmail.com>
2023-03-06 20:00:41 +0530

    fifth commit

---
+++ ex9.py
@@ -0,0 +1,8 @@
+#program to print a multiplication table for a given number
+
+n=int(input("enter n value: "))
+print("multiplication of given number is")
+for i in range(1,11):
+	print(n ,"*", i, "=", n*i)
+
+	
